2f76e9b4276f668c64225f87edd5a149f8efc860
kotharisoumya24 <43292726+kotharisoumya24@users.noreply.github.com>
2019-04-09 20:08:34 +0530

    Add files via upload

---
+++ week1-2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a and b";
+	cin>>a>>b;
+	int result;
+	result= a-(-b);
+	cout<<"\na + b = "<<result;
+	return 0;
+}
+
